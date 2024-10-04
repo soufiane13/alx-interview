12448f2eabfafd6a87c72facd960c51224972fcc
soufiane benhabboul <soufiane0bn@gmail.com>
2024-10-03 17:30:40 -0700

    ready to check

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,42 +1,25 @@
 #!/usr/bin/python3
-
 def pascal_triangle(n):
     """
-    This function will generate a Pascal Triangle up to the given number of
-    rows. The output will be a list of lists.
-
-    :param n: The number of rows to generate
-    :type n: int
-    :return: A list of lists representing the Pascal Triangle
-    :rtype: list
+    This function takes an integer n as input and returns the
+    first n lines of Pascal's triangle as a list of lists.
     """
-    k = list()
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """
+    This function takes an integer n as input and returns the
+    first n lines of Pascal's triangle as a list of lists.
+    """
 
-    if n <= 0:
-        return k
-
-    if n > 0:
-        # The first row is always [1]
-        k.append([1])
-
-    if n > 1:
-        # The second row is always [1, 1]
-        k.append([1, 1])
-
-    for x in range(3, n+1):
-        # For each row, we need to create a list of the correct size
-        k.append([0] * x)
-
-        # The first and last elements of each row are always 1
-        k[x-1][0] = 1
-        k[x-1][x-1] = 1
-
-        # For each element in the row (except the first and last)
-        for y in range(1, x-1):
-            # The value is the sum of the two elements directly above it
-            k[x-1][y] = k[x-2][y-1] + k[x-2][y]
-            k[x-1][y] = \
-                k[x-2][y-1] + k[x-2][y]
-
-    return k
-
+    triangle_pascal = []
+    if type(n) is not int or n <= 0:
+        return triangle_pascal
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle_pascal[i - 1][j - 1] + triangle_pascal[i - 1][j])
+        triangle_pascal.append(line)
+    return triangle_pascal
