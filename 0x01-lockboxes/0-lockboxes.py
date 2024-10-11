d82d871565ef32f7e95bb53e05fa4d5cf0eaccb0
SOUFIANE BENHABBOUL <36241651+soufiane13@users.noreply.github.com>
2024-10-11 01:14:09 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
 
+"""
+Problem: You have n number of locked boxes in front of you.
+         Each box is numbered sequentially from 0 to n - 1
+         and each box may contain keys to the other boxes.
+Task: Write a method that determines if all the boxes can be opened.
+"""
+
+
 def canUnlockAll(boxes):
     """
     This function will check if all boxes can be open.
-
-    Parameters
-    ----------
-    boxes : list
-        A list of lists where each sublist contains the keys of the boxes that
-        can be opened with the key in the current box.
-
-    Returns
-    -------
-    bool
-        True if all boxes can be opened and False otherwise.
     """
-    if not isinstance(boxes, list):
+    if type(boxes) is not list:
         return False
-    elif len(boxes) == 0:
+    elif (len(boxes)) == 0:
         return False
-
-    # Check if all boxes can be open
     for k in range(1, len(boxes) - 1):
-        checked_boxes = False
+        boxes_checked = False
         for idx in range(len(boxes)):
-            # Check if the key in the current box is in one of the other boxes
-            if k in boxes[idx] and k != idx:
-                checked_boxes = True
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
                 break
-        # If not, return False
-        if not checked_boxes:
-            return False
-
+        if boxes_checked is False:
+            return boxes_checked
     return True
